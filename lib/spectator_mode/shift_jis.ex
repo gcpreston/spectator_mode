b8f6e118dc4856446f9d3ea30cff1f8f915572c4
Graham Preston <graham.preston@gmail.com>
2025-04-16 12:53:03 +0200

    WIP decode Shift JIS

---
+++ lib/spectator_mode/shift_jis.ex
@@ -0,0 +1,13 @@
+defmodule SpectatorMode.ShiftJis do
+  @shift_jis "VENDORS/MICSFT/WINDOWS/CP932"
+
+  @doc ~S"""
+      iex> test = ShiftJis.encode("テスト")
+      <<131, 101, 131, 88, 131, 103>>
+      iex> ShiftJis.decode(test)
+      "テスト"
+  """
+
+  def encode(str), do: Codepagex.from_string!(str, @shift_jis)
+  def decode(str), do: Codepagex.to_string!(str, @shift_jis)
+end
