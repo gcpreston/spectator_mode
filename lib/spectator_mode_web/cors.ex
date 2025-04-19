cf8aab3e1dece526f9389d84af4ed8bc4d960786
Graham Preston <graham.preston@gmail.com>
2025-04-19 08:56:41 +0200

    Allow CORS headers

--- lib/spectator_mode_web/cors.ex
+++ lib/spectator_mode_web/cors.ex
@@ -2,5 +2,5 @@ defmodule SpectatorModeWeb.CORS do
   use Corsica.Router, origins: []
 
   resource "/*"
-  resource "/assets/zips/*", origins: "*"
+  resource "/assets/zips/*", origins: "*", allow_headers: :all
 end
