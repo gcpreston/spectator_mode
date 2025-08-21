5c3c94768e978c7753f9512ca7cefe5820089b86
Graham Preston <graham.preston@gmail.com>
2025-08-21 10:54:04 +0200

    Double-check for ETS data leak

--- test/spectator_mode/game_tracker_test.exs
+++ test/spectator_mode/game_tracker_test.exs
@@ -9,17 +9,22 @@ defmodule SpectatorMode.GameTrackerTest do
     game_start = EventsFixtures.game_start_fixture()
     fod_platform = EventsFixtures.fod_platforms_fixture()
 
+    # Initialize stream and events
     stream_id = GameTracker.initialize_stream()
     GameTracker.set_event_payloads(stream_id, event_payloads)
     GameTracker.set_game_start(stream_id, game_start)
     GameTracker.set_fod_platform(stream_id, fod_platform.platform, fod_platform)
 
+    # List tracked data
     assert GameTracker.list_streams() |> Enum.filter(fn %{stream_id: test_stream_id} -> test_stream_id == stream_id end) == [%{stream_id: stream_id, active_game: game_start}]
     assert GameTracker.join_payload(stream_id) == event_payloads.binary <> game_start.binary <> fod_platform.binary
 
+    # Delete and re-list
     GameTracker.delete(stream_id)
-
     assert GameTracker.list_streams() |> Enum.filter(fn %{stream_id: test_stream_id} -> test_stream_id == stream_id end) |> Enum.empty?()
     assert GameTracker.join_payload(stream_id) == <<>>
+
+    # Manually check that no keys remain
+    assert :ets.select(:livestreams, [{{{stream_id, :_}, :"$1"}, [], [:"$1"]}]) |> length() == 0
   end
 end
