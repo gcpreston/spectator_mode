afa4438bb4640f8dd76f8d619c63234367b9004b
Graham Preston <graham.preston@gmail.com>
2025-09-09 14:14:57 +0200

    Fix bridge tracker and game tracker tests

--- test/spectator_mode/game_tracker_test.exs
+++ test/spectator_mode/game_tracker_test.exs
@@ -1,6 +1,7 @@
 defmodule SpectatorMode.GameTrackerTest do
   use ExUnit.Case, async: false
 
+  alias SpectatorMode.Events
   alias SpectatorMode.GameTracker
   alias SpectatorMode.Slp.EventsFixtures
 
@@ -22,19 +23,22 @@ defmodule SpectatorMode.GameTrackerTest do
     game_start = EventsFixtures.game_start_fixture()
     fod_platform = EventsFixtures.fod_platforms_fixture()
 
-    # Initialize stream and events
+    # Initialize and check initial data
     stream_id = GameTracker.initialize_stream()
-    GameTracker.set_event_payloads(stream_id, event_payloads)
-    GameTracker.set_game_start(stream_id, game_start)
-    GameTracker.set_fod_platform(stream_id, fod_platform.platform, fod_platform)
+    assert GameTracker.list_local_streams() |> Enum.filter(fn %{stream_id: test_stream_id} -> test_stream_id == stream_id end) == [%{stream_id: stream_id, game_start: nil}]
+    assert GameTracker.minimal_join_payload(stream_id) == <<>>
 
-    # List tracked data
-    assert GameTracker.list_streams() |> Enum.filter(fn %{stream_id: test_stream_id} -> test_stream_id == stream_id end) == [%{stream_id: stream_id, game_start: game_start}]
+    # Check data after processing packets
+    GameTracker.handle_packet(stream_id, event_payloads.binary)
+    GameTracker.handle_packet(stream_id, game_start.binary)
+    GameTracker.handle_packet(stream_id, fod_platform.binary)
+    assert_receive %Events.GameStart{stream_id: ^stream_id, game_start: ^game_start}
+    assert GameTracker.list_local_streams() |> Enum.filter(fn %{stream_id: test_stream_id} -> test_stream_id == stream_id end) == [%{stream_id: stream_id, game_start: game_start}]
     assert GameTracker.minimal_join_payload(stream_id) == event_payloads.binary <> game_start.binary <> fod_platform.binary
 
-    # Delete and re-list
+    # Check data after removing stream
     GameTracker.delete(stream_id)
-    assert GameTracker.list_streams() |> Enum.filter(fn %{stream_id: test_stream_id} -> test_stream_id == stream_id end) |> Enum.empty?()
+    assert GameTracker.list_local_streams() |> Enum.filter(fn %{stream_id: test_stream_id} -> test_stream_id == stream_id end) |> Enum.empty?()
     assert GameTracker.minimal_join_payload(stream_id) == <<>>
 
     # Manually check that no keys remain
